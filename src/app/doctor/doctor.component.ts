1166b1ff367c00f10ec5f25c03b27e02e23454bd
annajjj <ajonak@o2.pl>
2017-08-22 09:19:02 +0200

    newversion

--- src/app/doctor/doctor.component.ts
+++ src/app/doctor/doctor.component.ts
@@ -2,17 +2,18 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AppService } from 'app/app.service';
 import { SharedModule } from '../shared/shared.module';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
-    templateUrl:    'doctor.component.html',
+  templateUrl:    'doctor.component.html',
   styleUrls: ['./doctor-style.css', '../shared/layout.css']
 })
 
 export class DoctorComponent implements OnInit{
   login: string;
   errorMessage: any;
-  visits;
-
+  visits:any;
+  visitsArray = new Array(5);
   constructor(private router: Router,
     private appService: AppService,
     private route: ActivatedRoute) {
@@ -20,9 +21,14 @@ export class DoctorComponent implements OnInit{
 
   ngOnInit(): void {
    this.login = this.route.snapshot.params['login'];
-   this.appService.findVisits({login: this.login}, 5)
-    .subscribe(response => this.visits = response);
+   this.getVisits()
+    .then(param => this.visits = param)
   }
+
+  getVisits(){
+       return this.appService.findVisits({login: this.login}, 5).toPromise()
+  }
+
 
 
 
